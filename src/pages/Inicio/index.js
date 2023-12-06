f0f523563bc1a6342e5ac47a8e71218c72ca33b4
Edu-dot-Silva <eduardo_esilva@hotmail.com>
2023-12-06 16:02:42 -0300

    primeiro commit

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,15 @@
+import Cabecalho from "componentes/Cabecalho"
+import Rodape from "componentes/Rodape/index"
+
+function Inicio(){
+    return(
+        <>
+        <Cabecalho/>
+
+        
+        <Rodape/>
+        </>
+    )
+}
+
+export default Inicio
